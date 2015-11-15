b082b20365e08e296777a3c587d80203ec99dc6e
Kelly Byrne <kellyannbyrne@utexas.edu>
2015-11-15 13:17:29 -0700

    external date and pluralized comments

--- public/javascripts/reddit.js
+++ public/javascripts/reddit.js
@@ -1,4 +1,4 @@
-var myApp = angular.module("myApp",[]);
+var myApp = angular.module("myApp",['angularMoment', 'ngAnimate']);
 		myApp.controller('Controller', function($scope) {
 			$scope.posts = [ ];
 			$scope.sortOrder = '-arrows';

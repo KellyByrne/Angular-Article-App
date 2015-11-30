0ae9c0f647430dfa33afe728f53fe1c2b64dd9dd
Kelly Byrne <kellyannbyrne@utexas.edu>
2015-11-30 14:45:41 -0700

    improved

--- public/javascripts/reddit.js
+++ public/javascripts/reddit.js
@@ -1,11 +1,17 @@
 var myApp = angular.module("myApp",['angularMoment', 'ngAnimate']);
 		myApp.controller('Controller', function($scope) {
-			$scope.posts = [ ];
-			$scope.sortOrder = '-arrows';
-			$scope.showcomm = false;
+
+		$scope.posts = [
+			{title:"foo", author: "bar", image:"http://i.imgur.com/O6Y76rA.jpg", date: new Date(), description: "blah blah blah", comments: [], showcomm: false},
+			{title:"foo", author: "bar", image:"http://i.imgur.com/O6Y76rA.jpg", date: new Date(), description: "blah blah blah", comments: [], showcomm: false},
+		];
+
+		$scope.sortOrder = '-arrows';
+	
+
   	$scope.addPost = function(isValid){
   		if(isValid){
-  		$scope.posts.push({title: $scope.title, author: $scope.author, image:$scope.image, date: new Date(), description: $scope.description, arrows: 0, comments:[]})
+  		$scope.posts.push({title: $scope.title, author: $scope.author, image:$scope.image, date: new Date(), description: $scope.description, arrows: 0, comments:[], showcomm: false})
   		$scope.title= '';
   		$scope.author='';
   		$scope.image ='';
@@ -31,25 +37,26 @@ var myApp = angular.module("myApp",['angularMoment', 'ngAnimate']);
 			}
 		}
 
-		$scope.openAdd = function(){
-			$scope.addcomm = true;
+		$scope.openAdd = function(post){
+			post.showCommentForm = true;
 		}
 
-		$scope.closeAdd = function(){
-			$scope.addcomm = false;
+		$scope.closeAdd = function(post){
+			post.showCommentForm = false;
 		}
 
 		$scope.addComment= function(post){
 	 		post.post.comments.push(this.elem)
 	 		console.log({object: post.post.comments})
 	 		this.elem= {};
+	 		post.post.showCommentForm = false;
 		}
 
-		$scope.showComments = function(){
-			if($scope.showcomm == false){
-				$scope.showcomm = true;
-			} else if($scope.showcomm == true) {
-				$scope.showcomm = false;
+		$scope.showComments = function(post){
+			if(post.showcomm == false){
+				post.showcomm = true;
+			} else if(post.showcomm == true) {
+				post.showcomm = false;
 			}
 		}
 
